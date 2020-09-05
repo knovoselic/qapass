99b689bdf64fcadf2764fb4efbc8a550a2737718
Kristijan Novoselić <kristijan@codecons.com>
2020-09-05 18:54:27 +0200

    Allow only registration with certain emails

--- src/tests/unit/validators/email.test.ts
+++ src/tests/unit/validators/email.test.ts
@@ -11,9 +11,29 @@ describe('email', () => {
             expect(email(req, 'rnd', 'test@test')).toBe(false);
         });
     });
-    describe('when subject is a string of valid email format', () => {
+    describe('when subject is an email ending with @cOdEcoNs.com', () => {
         it("returns true", async () => {
-            expect(email(req, 'rnd', 'test@test.com')).toBe(true);
+            expect(email(req, 'rnd', 'test@cOdEcoNs.com')).toBe(true);
+        });
+    });
+    describe('when subject is an email ending with @codecons.com', () => {
+        it("returns true", async () => {
+            expect(email(req, 'rnd', 'test@codecons.com')).toBe(true);
+        });
+    });
+    describe('when subject is an email ending with @glooko.com', () => {
+        it("returns true", async () => {
+            expect(email(req, 'rnd', 'test@glooko.com')).toBe(true);
+        });
+    });
+    describe('when subject is an email ending with @gmail.com', () => {
+        it("returns false", async () => {
+            expect(email(req, 'rnd', 'test@gmail.com')).toBe(false);
+        });
+    });
+    describe('when subject is an email ending with @test.com', () => {
+        it("returns false", async () => {
+            expect(email(req, 'rnd', 'test@test.com')).toBe(false);
         });
     });
 });
--- src/validators/email.ts
+++ src/validators/email.ts
@@ -4,7 +4,13 @@ export default (req: Request, field: string, subject: any) => {
 
     if(typeof subject !== 'string') return false;
 
+    subject = subject.toLowerCase();
+
+    if (!subject.endsWith("@codecons.com") && !subject.endsWith("@glooko.com")) {
+        return false;
+    }
+
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return regex.test(String(subject).toLowerCase());
+    return regex.test(subject);
 }
